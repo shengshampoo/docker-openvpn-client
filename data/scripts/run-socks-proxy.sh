4e9e4ab9e4606fac598481228b3ded3371776e6e
davy <cundaiyong@metrodata.cn>
2022-07-27 10:41:18 +0800

    update socks proxy config

--- data/scripts/run-socks-proxy.sh
+++ data/scripts/run-socks-proxy.sh
@@ -14,11 +14,11 @@ fi
 if [[ $SOCKS_PROXY_USERNAME && $SOCKS_PROXY_PASSWORD ]]; then
     printf 'info: starting socks proxy with credentials\n'
     useradd "$SOCKS_PROXY_USERNAME" -s /bin/false -M -p "$(mkpasswd "$SOCKS_PROXY_PASSWORD")"
-    sed -i "/method: /c method: username" $proxy_config_file
+    sed -i "/socksmethod: /c socksmethod: username" $proxy_config_file
 elif [[ -f "/run/secrets/$SOCKS_PROXY_USERNAME_SECRET" && -f "/run/secrets/$SOCKS_PROXY_PASSWORD_SECRET" ]]; then
     printf 'info: starting socks proxy with credentials\n'
     useradd "$(cat /run/secrets/"$SOCKS_PROXY_USERNAME_SECRET")" -s /bin/false -M -p "$(mkpasswd "$(cat /run/secrets/"$SOCKS_PROXY_PASSWORD_SECRET")")"
-    sed -i "/method: /c method: username" $proxy_config_file
+    sed -i "/socksmethod: /c socksmethod: username" $proxy_config_file
 else
     printf 'info: starting socks proxy without credentials\n'
 fi
